01813c608d4134a26ce8aa6343ef98b881a65324
JD Benito <jd@hybridforge.com>
2017-10-26 17:11:01 -0600

    FOL-176 Basic implementation of the CRUD APIs for PO Lines
    TODO: Testing

---
+++ src/main/java/org/folio/rest/impl/POLineAPI.java
@@ -0,0 +1,304 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.jaxrs.model.PoLine;
+import org.folio.rest.jaxrs.model.PoLineCollection;
+import org.folio.rest.jaxrs.resource.PoLineResource;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.messages.MessageConsts;
+import org.folio.rest.tools.messages.Messages;
+import org.folio.rest.tools.utils.OutStream;
+import org.folio.rest.tools.utils.TenantTool;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class POLineAPI implements PoLineResource {
+  private static final String PO_LINE_TABLE = "purchase_order_line";
+  private static final String PO_LINE_LOCATION = "/po_line/";
+
+  private static final Logger log = LoggerFactory.getLogger(PurchaseOrderAPI.class);
+  private final Messages messages = Messages.getInstance();
+  private String idFieldName = "_id";
+
+
+  private org.folio.rest.persist.Criteria.Order getOrder(Order order, String field) {
+    if (field == null) {
+      return null;
+    }
+
+    String sortOrder = org.folio.rest.persist.Criteria.Order.ASC;
+    if (order.name().equals("desc")) {
+      sortOrder = org.folio.rest.persist.Criteria.Order.DESC;
+    }
+
+    String fieldTemplate = String.format("jsonb->'%s'", field);
+    return new org.folio.rest.persist.Criteria.Order(fieldTemplate, org.folio.rest.persist.Criteria.Order.ORDER.valueOf(sortOrder.toUpperCase()));
+  }
+
+  @Override
+  public void getPoLine(String query, String orderBy, Order order, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
+
+        Criterion criterion = Criterion.json2Criterion(query);
+//        Criteria criteria = new Criteria("/ramls/schemas/" +PURCHASE_ORDER_TABLE + ".json");
+//        criterion.addCriterion(criteria);
+        criterion.setLimit(new Limit(limit));
+        criterion.setOffset(new Offset(offset));
+
+        org.folio.rest.persist.Criteria.Order or = getOrder(order, orderBy);
+        if (or != null) {
+          criterion.setOrder(or);
+        }
+
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(PO_LINE_TABLE, PoLine.class, criterion, true,
+          reply -> {
+            try {
+              if(reply.succeeded()){
+                PoLineCollection collection = new PoLineCollection();
+                @SuppressWarnings("unchecked")
+                List<PoLine> resultSet = (List<PoLine>) reply.result().getResults();
+                collection.setPoLines(resultSet);
+                Integer totalRecords = reply.result().getResultInfo().getTotalRecords();
+                collection.setTotalRecords(totalRecords);
+                Integer first = 0, last = 0;
+                if (resultSet.size() > 0) {
+                  first = offset + 1;
+                  last = offset + resultSet.size();
+                }
+                collection.setFirst(first);
+                collection.setLast(last);
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineResponse.withJsonOK(
+                  collection)));
+              }
+              else{
+                log.error(reply.cause().getMessage(), reply.cause());
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineResponse
+                  .withPlainBadRequest(reply.cause().getMessage())));
+              }
+            } catch (Exception e) {
+              log.error(e.getMessage(), e);
+              asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineResponse
+                .withPlainInternalServerError(messages.getMessage(
+                  lang, MessageConsts.InternalServerError))));
+            }
+          });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        String message = messages.getMessage(lang, MessageConsts.InternalServerError);
+        if(e.getCause() != null && e.getCause().getClass().getSimpleName().endsWith("CQLParseException")) {
+          message = " CQL parse error " + e.getLocalizedMessage();
+        }
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineResponse
+          .withPlainInternalServerError(message)));
+      }
+    });
+  }
+
+  @Override
+  public void postPoLine(String lang, PoLine entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+    vertxContext.runOnContext(v -> {
+
+      try {
+        String id = UUID.randomUUID().toString();
+        if(entity.getId() == null){
+          entity.setId(id);
+        }
+        else{
+          id = entity.getId();
+        }
+
+        String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).save(
+          PO_LINE_TABLE, id, entity,
+          reply -> {
+            try {
+              if (reply.succeeded()) {
+                String persistenceId = reply.result();
+                entity.setId(persistenceId);
+                OutStream stream = new OutStream();
+                stream.setData(entity);
+
+                Response response = PostPoLineResponse.withJsonCreated(PO_LINE_LOCATION + persistenceId, stream);
+                respond(asyncResultHandler, response);
+              }
+              else {
+                log.error(reply.cause().getMessage(), reply.cause());
+                Response response = PostPoLineResponse.withPlainInternalServerError(reply.cause().getMessage());
+                respond(asyncResultHandler, response);
+              }
+            }
+            catch (Exception e) {
+              log.error(e.getMessage(), e);
+
+              Response response = PostPoLineResponse.withPlainInternalServerError(e.getMessage());
+              respond(asyncResultHandler, response);
+            }
+
+          }
+        );
+      }
+      catch (Exception e) {
+        log.error(e.getMessage(), e);
+
+        String errMsg = messages.getMessage(lang, MessageConsts.InternalServerError);
+        Response response = PostPoLineResponse.withPlainInternalServerError(errMsg);
+        respond(asyncResultHandler, response);
+      }
+
+    });
+  }
+
+  @Override
+  public void getPoLineByPoLineId(String poLineId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
+
+        String idArgument = String.format("'%s'", poLineId);
+        Criterion c = new Criterion(
+          new Criteria().addField(idFieldName).setJSONB(false).setOperation("=").setValue(idArgument));
+
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(PO_LINE_TABLE, PoLineCollection.class, c, true,
+          reply -> {
+            try {
+              if(reply.succeeded()){
+                @SuppressWarnings("unchecked")
+                List<PoLine> userGroup = (List<PoLine>) reply.result().getResults();
+                if(userGroup.isEmpty()){
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineByPoLineIdResponse
+                    .withPlainNotFound(poLineId)));
+                }
+                else{
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineByPoLineIdResponse
+                    .withJsonOK(userGroup.get(0))));
+                }
+              }
+              else{
+                log.error(reply.cause().getMessage(), reply.cause());
+                if(isInvalidUUID(reply.cause().getMessage())){
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineByPoLineIdResponse
+                    .withPlainNotFound(poLineId)));
+                }
+                else{
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineByPoLineIdResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+                }
+              }
+            } catch (Exception e) {
+              log.error(e.getMessage(), e);
+              asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineByPoLineIdResponse
+                .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+            }
+          });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetPoLineByPoLineIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+      }
+    });
+
+  }
+
+  @Override
+  public void deletePoLineByPoLineId(String poLineId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+
+    String tenantId = TenantTool.tenantId(okapiHeaders);
+    try {
+      vertxContext.runOnContext(v -> {
+        PostgresClient postgresClient = PostgresClient.getInstance(
+          vertxContext.owner(), TenantTool.calculateTenantId(tenantId));
+
+        try {
+          postgresClient.delete(PO_LINE_TABLE, poLineId, reply -> {
+            if (reply.succeeded()) {
+              asyncResultHandler.handle(Future.succeededFuture(
+                PoLineResource.DeletePoLineByPoLineIdResponse
+                  .noContent().build()));
+            } else {
+              asyncResultHandler.handle(Future.succeededFuture(
+                PoLineResource.DeletePoLineByPoLineIdResponse
+                  .withPlainInternalServerError(reply.cause().getMessage())));
+            }
+          });
+        } catch (Exception e) {
+          asyncResultHandler.handle(Future.succeededFuture(
+            PoLineResource.DeletePoLineByPoLineIdResponse
+              .withPlainInternalServerError(e.getMessage())));
+        }
+      });
+    }
+    catch(Exception e) {
+      asyncResultHandler.handle(Future.succeededFuture(
+        PoLineResource.DeletePoLineByPoLineIdResponse
+          .withPlainInternalServerError(e.getMessage())));
+    }
+  }
+
+  @Override
+  public void putPoLineByPoLineId(String poLineId, String lang, PoLine entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+
+    vertxContext.runOnContext(v -> {
+      String tenantId = TenantTool.calculateTenantId( okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT) );
+      try {
+        if(entity.getId() == null){
+          entity.setId(poLineId);
+        }
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).update(
+          PO_LINE_TABLE, entity, poLineId,
+          reply -> {
+            try {
+              if(reply.succeeded()){
+                if(reply.result().getUpdated() == 0){
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutPoLineByPoLineIdResponse
+                    .withPlainNotFound(messages.getMessage(lang, MessageConsts.NoRecordsUpdated))));
+                }
+                else{
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutPoLineByPoLineIdResponse
+                    .withNoContent()));
+                }
+              }
+              else{
+                log.error(reply.cause().getMessage());
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutPoLineByPoLineIdResponse
+                  .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+              }
+            } catch (Exception e) {
+              log.error(e.getMessage(), e);
+              asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutPoLineByPoLineIdResponse
+                .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+            }
+          });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutPoLineByPoLineIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+      }
+    });
+    
+  }
+
+  private static void respond(Handler<AsyncResult<Response>> handler, Response response) {
+    AsyncResult<Response> result = Future.succeededFuture(response);
+    handler.handle(result);
+  }
+
+  private boolean isInvalidUUID (String errorMessage) {
+    return (errorMessage != null && errorMessage.contains("invalid input syntax for uuid"));
+  }
+}
