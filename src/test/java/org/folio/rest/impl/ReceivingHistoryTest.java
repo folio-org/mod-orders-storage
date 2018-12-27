29bddd3e6c7db8138e375eeca4c53a4e6222598c
javalkar-varun <javalkar.varun@gmail.com>
2018-12-27 11:41:50 -0500

    Add newline

--- src/test/java/org/folio/rest/impl/ReceivingHistoryTest.java
+++ src/test/java/org/folio/rest/impl/ReceivingHistoryTest.java
@@ -14,16 +14,16 @@ import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 @RunWith(VertxUnitRunner.class)
-public class ReceivingHistoryTest extends OrdersStorageTest{
-  
+public class ReceivingHistoryTest extends OrdersStorageTest {
+
   private final Logger logger = LoggerFactory.getLogger("okapi");
 
   private String piecesSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
   private String detailSampleId;
   private String poLineSampleId;
-  
-  private static final String RECEIVING_HISTORY_ENDPOINT ="/orders-storage/receiving_history";
-  private static final String PIECES_ENDPOINT ="/orders-storage/pieces";
+
+  private static final String RECEIVING_HISTORY_ENDPOINT = "/orders-storage/receiving_history";
+  private static final String PIECES_ENDPOINT = "/orders-storage/pieces";
   private static final String PO_LINE_ENDPOINT = "/orders-storage/po_lines";
   private final static String DETAILS_ENDPOINT = "/orders-storage/details";
 
@@ -39,22 +39,22 @@ public class ReceivingHistoryTest extends OrdersStorageTest{
       verifyViewCollection(RECEIVING_HISTORY_ENDPOINT);
 
       logger.info("--- mod-orders-storage receiving_history test: Creating receiving_history view ...");
-      
+
       logger.info("--- mod-orders-storage receiving_history test: Creating Pieces ...");
-      testCreatePiece();      
+      testCreatePiece();
       testVerifyPieceCreated();
-      
+
       logger.info("--- mod-orders-storage receiving_history test: Creating PoLine ...");
       testCreatePoLine();
       testVerifyPoLineCreated();
-      
+
       logger.info("--- mod-orders-storage receiving_history test: Creating Detail ...");
       testCreateDetail();
       testVerifyDetailCreated();
-      
+
       logger.info("--- mod-orders-storage pieces test: Verify receiving_history view created ...");
       verifyReceivingHistoryViewCreated(RECEIVING_HISTORY_ENDPOINT);
-      
+
     } catch (Exception e) {
       logger.error("--- mod-orders-storage-test: receiving_history API ERROR: " + e.getMessage(), e);
     } finally {
@@ -64,7 +64,7 @@ public class ReceivingHistoryTest extends OrdersStorageTest{
       testDeletePieces(piecesSampleId);
     }
   }
-  
+
   private void testDeleteDetail(String detailSampleId) {
     deleteData(DETAILS_ENDPOINT, detailSampleId).then().log().ifValidationFails()
     .statusCode(204);
@@ -85,7 +85,7 @@ public class ReceivingHistoryTest extends OrdersStorageTest{
     Response detailResponse = postData(DETAILS_ENDPOINT, detailSample);
     detailSampleId = detailResponse.then().extract().path("id");
   }
-  
+
   private void testCreatePiece() {
     String pieceSample = getFile("pieces_for_view.sample");
     Response response = postData(PIECES_ENDPOINT, pieceSample);
@@ -93,14 +93,12 @@ public class ReceivingHistoryTest extends OrdersStorageTest{
   }
   
   private void testCreatePoLine() {
-    logger.info("--- mod-storage-test: Creating PO line ... ");
     String poLineSample = getFile("po_line_for_view.sample");
     Response response = postData(PO_LINE_ENDPOINT, poLineSample);
     response.then().log().ifValidationFails()
       .statusCode(201)
       .body("description", equalTo("ABCDEFGH"));
     poLineSampleId = response.then().extract().path("id");
-    logger.info("-- poLineSampleId ------> " + poLineSampleId);
   }
   
   private void testVerifyDetailCreated() {
@@ -143,5 +141,4 @@ public class ReceivingHistoryTest extends OrdersStorageTest{
       .contentType(ContentType.JSON)
       .get(endpoint);
   }
-
 }
