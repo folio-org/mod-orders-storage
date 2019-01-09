fee35150f0ff402875abe2f20171d532c6ebe1f4
javalkar-varun <vjavalkar@ebsco.com>
2019-01-08 20:28:27 -0500

    Clean-up code

--- src/test/java/org/folio/rest/impl/ReceivingHistoryTest.java
+++ src/test/java/org/folio/rest/impl/ReceivingHistoryTest.java
@@ -18,18 +18,25 @@ public class ReceivingHistoryTest extends OrdersStorageTest {
 
   private final Logger logger = LoggerFactory.getLogger("okapi");
 
-  private String piecesSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
-  private String detailSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
-  private String poLineSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
-  private String piecesSampleId2; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
-  private String detailSampleId2; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
-  private String poLineSampleId2; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
+  private static String piecesSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
+  private static String detailSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
+  private static String poLineSampleId; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
+  private static String piecesSampleId2; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
+  private static String detailSampleId2; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
+  private static String poLineSampleId2; // "2303926f-0ef7-4063-9039-07c0e7fae77d"
 
   private static final String RECEIVING_HISTORY_ENDPOINT = "/orders-storage/receiving_history";
   private static final String PIECES_ENDPOINT = "/orders-storage/pieces";
   private static final String PO_LINE_ENDPOINT = "/orders-storage/po_lines";
-  private final static String DETAILS_ENDPOINT = "/orders-storage/details";
+  private static final String DETAILS_ENDPOINT = "/orders-storage/details";
 
+  private final String detailSample = getFile("details.sample");
+  private final String detailSample2 = getFile("detail_for_view.sample");
+  private final String poLineSample = getFile("po_line.sample");
+  private final String poLineSample2 = getFile("po_line_for_view.sample");
+  private final String pieceSample = getFile("pieces.sample");
+  private final String pieceSample2 = getFile("piece_for_view.sample");
+  
   @Test
   public void testReceivingHistory() {
     try {
@@ -37,36 +44,32 @@ public class ReceivingHistoryTest extends OrdersStorageTest {
       logger.info("--- mod-orders-storage receiving_history test: Preparing test tenant ...");
       prepareTenant();
 
-      logger.info("--- mod-orders-storage receiving_history test: Verifying database's initial state ... ");
+      logger.info("--- mod-orders-storage receiving_history test: Before receiving_history View creation ... ");
       verifyViewCollectionBefore(RECEIVING_HISTORY_ENDPOINT, 0);
 
-      logger.info("--- mod-orders-storage receiving_history test: Creating receiving_history view ...");
-
-      logger.info("--- mod-orders-storage receiving_history test: Creating Pieces 1...");
-      testCreatePiece();
+      logger.info("--- mod-orders-storage receiving_history test: Creating receiving_history View ...");
+      logger.info("--- mod-orders-storage receiving_history test: Creating Piece 1...");
+      piecesSampleId = testCreatePiece(pieceSample);
+      logger.info("--- mod-orders-storage receiving_history test: Creating Piece 2 ...");
+      piecesSampleId2 = testCreatePiece(pieceSample2);
       testVerifyPieceCreated();
-
-      logger.info("--- mod-orders-storage receiving_history test: Creating Pieces 2 ...");
-      testCreatePiece2();
-      testVerifyPieceCreated2();
       
+ 
       logger.info("--- mod-orders-storage receiving_history test: Creating PoLine 1...");
-      testCreatePoLine();
-      testVerifyPoLineCreated();
-
+      poLineSampleId = testCreatePoLine(poLineSample);
       logger.info("--- mod-orders-storage receiving_history test: Creating PoLine 2 ...");
-      testCreatePoLine2();
-      testVerifyPoLineCreated2();
+      poLineSampleId2 = testCreatePoLine(poLineSample2);
+      testVerifyPoLineCreated();
+      
       
       logger.info("--- mod-orders-storage receiving_history test: Creating Detail 1...");
-      testCreateDetail();
-      testVerifyDetailCreated();
-
+      detailSampleId = testCreateDetail(detailSample);
       logger.info("--- mod-orders-storage receiving_history test: Creating Detail 2 ...");
-      testCreateDetail2();
-      testVerifyDetailCreated2();
-      
-      logger.info("--- mod-orders-storage pieces test: Verify receiving_history view created ...");
+      detailSampleId2 = testCreateDetail(detailSample2);
+      testVerifyDetailCreated();
+     
+  
+      logger.info("--- mod-orders-storage pieces test: After receiving_history View created ...");
       verifyViewCollectionAfter(RECEIVING_HISTORY_ENDPOINT, 2);
 
     } catch (Exception e) {
@@ -98,7 +101,6 @@ public class ReceivingHistoryTest extends OrdersStorageTest {
   }
   
   void verifyViewCollectionBefore(String endpoint, int expectedCount) {
-    logger.info("--- expectedCount -----> " + expectedCount);
     // Verify that there are no existing records in View
     getViewData(endpoint).then()
       .log().all()
@@ -106,55 +108,24 @@ public class ReceivingHistoryTest extends OrdersStorageTest {
       .body("total_records", equalTo(expectedCount));
   }
   
-  private void testCreateDetail() {
-    String detailSample = getFile("details.sample");
+  private String testCreateDetail(String detailSample) {
     Response detailResponse = postData(DETAILS_ENDPOINT, detailSample);
-    detailSampleId = detailResponse.then().extract().path("id");
-  }
-
-  private void testCreateDetail2() {
-    String detailSample2 = getFile("details_for_view.sample");
-    Response detailResponse = postData(DETAILS_ENDPOINT, detailSample2);
-    detailSampleId2 = detailResponse.then().extract().path("id");
+    return detailResponse.then().extract().path("id");
   }
   
-  private void testCreatePiece() {
-    String pieceSample = getFile("pieces.sample");
+  private String testCreatePiece(String pieceSample) {
     Response response = postData(PIECES_ENDPOINT, pieceSample);
-    piecesSampleId = response.then().extract().path("id");
+    return response.then().extract().path("id");
   }
   
-  private void testCreatePiece2() {
-    String pieceSample2 = getFile("piece_for_view.sample");
-    Response response = postData(PIECES_ENDPOINT, pieceSample2);
-    piecesSampleId2 = response.then().extract().path("id");
-  }
-  
-  private void testCreatePoLine() {
-    String poLineSample = getFile("po_line.sample");
+  private String testCreatePoLine(String poLineSample) {
     Response response = postData(PO_LINE_ENDPOINT, poLineSample);
     response.then().log().ifValidationFails()
-      .statusCode(201)
-      .body("description", equalTo("ABCDEFGH"));
-    poLineSampleId = response.then().extract().path("id");
+      .statusCode(201);
+    return response.then().extract().path("id");
   }
-  
-  private void testCreatePoLine2() {
-    String poLineSample2 = getFile("po_line_for_view.sample");
-    Response response = postData(PO_LINE_ENDPOINT, poLineSample2);
-    response.then().log().ifValidationFails()
-      .statusCode(201)
-      .body("description", equalTo("description for po_line receiving_history view"));
-    poLineSampleId2 = response.then().extract().path("id");
-  }
-  
+
   private void testVerifyDetailCreated() {
-    getData(DETAILS_ENDPOINT).then().log().ifValidationFails()
-    .statusCode(200)
-    .body("total_records", equalTo(17));
-  }
-  
-  private void testVerifyDetailCreated2() {
     getData(DETAILS_ENDPOINT).then().log().ifValidationFails()
     .statusCode(200)
     .body("total_records", equalTo(18));
@@ -163,30 +134,16 @@ public class ReceivingHistoryTest extends OrdersStorageTest {
   private void testVerifyPoLineCreated() {
     getData(PO_LINE_ENDPOINT).then().log().ifValidationFails()
     .statusCode(200)
-    .body("total_records", equalTo(17));
-  }
-  
-  private void testVerifyPoLineCreated2() {
-    getData(PO_LINE_ENDPOINT).then().log().ifValidationFails()
-    .statusCode(200)
     .body("total_records", equalTo(18));
   }
   
   private void testVerifyPieceCreated() {
     getData(PIECES_ENDPOINT).then().log().ifValidationFails()
     .statusCode(200)
-    .body("total_records", equalTo(1));
-  }
-  
-  private void testVerifyPieceCreated2() {
-    getData(PIECES_ENDPOINT).then().log().ifValidationFails()
-    .statusCode(200)
     .body("total_records", equalTo(2));
   }
   
   private void verifyViewCollectionAfter(String endpoint, int expectedCount) {
-    // Verify that there are no existing records in View
-	logger.info("--- expectedCount after view creation-----> " + expectedCount);
 	getViewData(endpoint).then()
 	  .log().all()
 	  .statusCode(200)
@@ -203,6 +160,8 @@ public class ReceivingHistoryTest extends OrdersStorageTest {
 	  .body("receiving_history[1].title", equalTo("Skiing in the Colorado"))
 	  .body("receiving_history[0].po_line_id", equalTo("d471d766-8dbb-4609-999a-02681dea6c22"))
 	  .body("receiving_history[1].po_line_id", equalTo("2fe6c2dd-3700-4a53-a624-1159cfd7f8ce"))
+	  .body("receiving_history[0].po_line_number", equalTo("268758-03"))
+	  .body("receiving_history[1].po_line_number", equalTo("268500-03"))
 	  .body("receiving_history[0].receiving_note", equalTo("ABCDEFGHIJKL"))
 	  .body("receiving_history[1].receiving_note", equalTo("details for view"));
   }
