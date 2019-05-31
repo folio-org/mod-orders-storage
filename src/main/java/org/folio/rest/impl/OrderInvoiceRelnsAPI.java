d945ff509d32ad48e25fdf832d2739ed8e90ad90
Raman Asadchy <Raman_Asadchy@epam.com>
2019-05-31 10:11:00 +0300

    Create unit tests for relationship between order and invoice

--- src/main/java/org/folio/rest/impl/OrderInvoiceRelnsAPI.java
+++ src/main/java/org/folio/rest/impl/OrderInvoiceRelnsAPI.java
@@ -3,13 +3,14 @@ package org.folio.rest.impl;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.OrderInvoiceRelationship;
 import org.folio.rest.jaxrs.model.OrderInvoiceRelationshipCollection;
-import org.folio.rest.jaxrs.model.PoLine;
 import org.folio.rest.jaxrs.resource.OrderStorageOrderInvoiceRelns;
-import org.folio.rest.jaxrs.resource.OrdersStoragePoLines;
 import org.folio.rest.persist.EntitiesMetadataHolder;
 import org.folio.rest.persist.PgUtil;
+import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.persist.QueryHolder;
 
 import javax.ws.rs.core.Response;
@@ -20,8 +21,14 @@ import static org.folio.rest.persist.HelperUtils.getEntitiesCollection;
 public class OrderInvoiceRelnsAPI implements OrderStorageOrderInvoiceRelns {
 
   private static final String ORDER_INVOICE_RELNS_TABLE = "order_invoice_relationship";
+  private String idFieldName = "id";
+
+  public OrderInvoiceRelnsAPI(Vertx vertx, String tenantId) {
+    PostgresClient.getInstance(vertx, tenantId).setIdField(idFieldName);
+  }
 
   @Override
+  @Validate
   public void getOrderStorageOrderInvoiceRelns(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     vertxContext.runOnContext((Void v) -> {
       EntitiesMetadataHolder<OrderInvoiceRelationship, OrderInvoiceRelationshipCollection> entitiesMetadataHolder = new EntitiesMetadataHolder<>(OrderInvoiceRelationship.class, OrderInvoiceRelationshipCollection.class, GetOrderStorageOrderInvoiceRelnsResponse.class);
@@ -31,23 +38,27 @@ public class OrderInvoiceRelnsAPI implements OrderStorageOrderInvoiceRelns {
   }
 
   @Override
+  @Validate
   public void postOrderStorageOrderInvoiceRelns(String lang, OrderInvoiceRelationship entity,
     Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.post(ORDER_INVOICE_RELNS_TABLE, entity, okapiHeaders, vertxContext, PostOrderStorageOrderInvoiceRelnsResponse.class, asyncResultHandler);
   }
 
   @Override
+  @Validate
   public void getOrderStorageOrderInvoiceRelnsById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     PgUtil.getById(ORDER_INVOICE_RELNS_TABLE, OrderInvoiceRelationship.class, id, okapiHeaders,vertxContext, GetOrderStorageOrderInvoiceRelnsByIdResponse.class, asyncResultHandler);
   }
 
   @Override
+  @Validate
   public void deleteOrderStorageOrderInvoiceRelnsById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.getById(ORDER_INVOICE_RELNS_TABLE, OrderInvoiceRelationship.class, id, okapiHeaders,vertxContext, DeleteOrderStorageOrderInvoiceRelnsByIdResponse.class, asyncResultHandler);
+    PgUtil.deleteById(ORDER_INVOICE_RELNS_TABLE, id, okapiHeaders, vertxContext, DeleteOrderStorageOrderInvoiceRelnsByIdResponse.class, asyncResultHandler);
   }
 
   @Override
+  @Validate
   public void putOrderStorageOrderInvoiceRelnsById(String id, String lang, OrderInvoiceRelationship entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    PgUtil.getById(ORDER_INVOICE_RELNS_TABLE, OrderInvoiceRelationship.class, id, okapiHeaders,vertxContext, PutOrderStorageOrderInvoiceRelnsByIdResponse.class, asyncResultHandler);
+    PgUtil.put(ORDER_INVOICE_RELNS_TABLE, entity, id, okapiHeaders,vertxContext, PutOrderStorageOrderInvoiceRelnsByIdResponse.class, asyncResultHandler);
   }
 }
