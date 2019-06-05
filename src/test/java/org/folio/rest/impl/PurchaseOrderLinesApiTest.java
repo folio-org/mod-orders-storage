3d6655420ca36cac3b1816ac09969d6f0685c2db
Raman Asadchy <Raman_Asadchy@epam.com>
2019-06-05 11:00:43 +0300

    Use TestEntities for test

--- src/test/java/org/folio/rest/impl/PurchaseOrderLinesApiTest.java
+++ src/test/java/org/folio/rest/impl/PurchaseOrderLinesApiTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import org.folio.rest.utils.TestEntities;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
@@ -11,18 +12,17 @@ import java.net.MalformedURLException;
 public class PurchaseOrderLinesApiTest extends TestBase {
 
   private final Logger logger = LoggerFactory.getLogger(OrdersAPITest.class);
-  private static final String PO_LINES_ENDPOINT = "/orders-storage/po-lines";
-
-  private final String poLineSample = getFile("data/po-lines/312325-1_fully_received_electronic_method-purchase_at_vendor_system.json");
 
   @Test
   public void testPostOrdersLinesByIdPoLineWithoutId() throws MalformedURLException {
     logger.info("--- mod-orders-storage orders test: post PoLine without purchaseOrderId");
 
-    JsonObject data = new JsonObject(poLineSample);
+    TestEntities testEntities = TestEntities.PO_LINE;
+    JsonObject data = new JsonObject(getFile(testEntities.getSampleFileName()));
+
     data.remove("purchaseOrderId");
 
-    Response response = postData(PO_LINES_ENDPOINT, data.toString());
+    Response response = postData(testEntities.getEndpoint(), data.toString());
     response.then()
       .statusCode(422);
   }
