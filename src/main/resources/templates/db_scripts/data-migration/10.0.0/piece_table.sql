cae3e799481e94b7662980392382ca72b8ece7c0
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-01-10 20:35:53 +0300

    MODORDSTOR-129 add receipt date to piece schema
    - removing migration script for pieces

--- src/main/resources/templates/db_scripts/data-migration/10.0.0/piece_table.sql
+++
@@ -1,7 +0,0 @@
-UPDATE ${myuniversity}_${mymodule}.pieces
-SET jsonb = jsonb || jsonb_build_object('receiptDate', jsonb->>'receivedDate')
-WHERE jsonb ? 'receivedDate' AND NOT jsonb ? 'receiptDate';
-
-UPDATE ${myuniversity}_${mymodule}.pieces
-SET jsonb = jsonb || jsonb_build_object('receiptDate', now())
-WHERE NOT jsonb ? 'receivedDate' AND NOT jsonb ? 'receiptDate';
