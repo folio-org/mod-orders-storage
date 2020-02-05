2e69e46e6be712e7cfa2a627760048d2b1be09af
Aliaksandr Pautau <aliaksandr_pautau@epam.com>
2020-02-05 16:12:51 +0300

    MODORDSTOR-128 Data migration scripts for schema changes (#171)
    - for po_line table rename title to titleOrPackage, delete instanceId, add isPackage
    - create title for each unprocessed poLine

---
+++ src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION "uuid-ossp";
+
+INSERT INTO ${myuniversity}_${mymodule}.titles
+SELECT uuid_generate_v4(), jsonb_strip_nulls(jsonb_build_object('title', po_line.jsonb -> 'title', 'poLineId', po_line.id, 'instanceId', po_line.jsonb-> 'instanceId',
+  'productIds', po_line.jsonb -> 'details' -> 'productIds', 'contributors', po_line.jsonb -> 'contributors', 'edition', po_line.jsonb -> 'edition',
+  'publisher', po_line.jsonb -> 'publisher', 'publishedDate', po_line.jsonb -> 'publicationDate', 'subscriptionFrom', po_line.jsonb -> 'details' -> 'subscriptionFrom',
+  'subscriptionTo', po_line.jsonb -> 'details' -> 'subscriptionTo', 'subscriptionInterval', po_line.jsonb -> 'details' -> 'subscriptionInterval'))
+FROM ${myuniversity}_${mymodule}.po_line AS po_line
+WHERE po_line.jsonb ? 'title';
+
+UPDATE ${myuniversity}_${mymodule}.po_line
+SET jsonb = jsonb - '{title, instanceId}'::text[] || jsonb_build_object('titleOrPackage', jsonb->'title', 'isPackage', false)
+WHERE jsonb ? 'title';
+
+DROP EXTENSION "uuid-ossp";
+
