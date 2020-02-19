15f68fae7ff39ad7536b92251937b61e6a254b51
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2020-02-19 23:58:12 +0300

    MODORDSTOR-131 added migration scripts

--- src/main/resources/templates/db_scripts/data-migration/10.0.0/purchase_order_table.sql
+++ src/main/resources/templates/db_scripts/data-migration/10.0.0/purchase_order_table.sql
@@ -1,5 +1,5 @@
 -- Rename the "renewal" field to "ongoing" in purchase_order table
-UPDATE diku_mod_orders_storage.purchase_order
+UPDATE ${myuniversity}_${mymodule}.purchase_order
 SET
   jsonb = jsonb #- '{renewal}' || jsonb_build_object('ongoing', jsonb::json -> 'renewal')
 WHERE
@@ -7,20 +7,20 @@ WHERE
 
 
 -- Remove ongoing.cycle field from purchase_order table
-UPDATE diku_mod_orders_storage.purchase_order
+UPDATE ${myuniversity}_${mymodule}.purchase_order
 SET
   jsonb = jsonb #- '{ongoing,cycle}';
 
 
 -- Update interval for existing data
-UPDATE diku_mod_orders_storage.purchase_order
+UPDATE ${myuniversity}_${mymodule}.purchase_order
 SET
   jsonb = jsonb_set(jsonb, '{ongoing,interval}', '365')
 WHERE jsonb #> '{ongoing, interval}' IS NOT NULL;
 
 
 --add "isSubscription" field for ongoing orders and set default value
-UPDATE diku_mod_orders_storage.purchase_order
+UPDATE ${myuniversity}_${mymodule}.purchase_order
 SET
   jsonb = jsonb_set(jsonb, '{ongoing, isSubscription}', 'false')
 WHERE (jsonb ? 'ongoing' AND jsonb #> '{ongoing, isSubscription}' IS NULL);
