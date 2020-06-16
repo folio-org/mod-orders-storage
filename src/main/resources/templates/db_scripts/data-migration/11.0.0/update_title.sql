d79d8209b341128acb09b4d71d56d1d3156f6f00
Aliaksandr Pautau <aliaksandr_pautau@epam.com>
2020-06-16 17:26:55 +0300

    MODORDSTOR-153 Package name populated for non-package titles during migration to 11.0.0 version (#193)

--- src/main/resources/templates/db_scripts/data-migration/11.0.0/update_title.sql
+++ src/main/resources/templates/db_scripts/data-migration/11.0.0/update_title.sql
@@ -1,9 +1,11 @@
 UPDATE ${myuniversity}_${mymodule}.titles as titles
-SET jsonb = jsonb ||
+SET jsonb = jsonb - 'packageName' ||
     (
       SELECT jsonb_strip_nulls(jsonb_build_object('poLineNumber', po_line.jsonb -> 'poLineNumber',
-								'receivingNote', po_line.jsonb -> 'details' -> 'receivingNote',
-								'expectedReceiptDate', po_line.jsonb -> 'physical' -> 'expectedReceiptDate',
-								'packageName', po_line.jsonb -> 'titleOrPackage'))
+								                                  'receivingNote', po_line.jsonb -> 'details' -> 'receivingNote',
+								                                  'expectedReceiptDate', po_line.jsonb -> 'physical' -> 'expectedReceiptDate',
+                                                  'packageName', CASE WHEN po_line.jsonb ? 'packagePoLineId' OR (po_line.jsonb->>'isPackage')::boolean
+												                                              THEN po_line.jsonb -> 'titleOrPackage'
+												                                         END))
       FROM  ${myuniversity}_${mymodule}.po_line as po_line WHERE po_line.id = titles.poLineId
     );
