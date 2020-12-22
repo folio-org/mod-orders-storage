a13a7efd5b007ab3db50c41f135bed9394c95df1
Aliaksandr Pautau <aliaksandr_pautau@epam.com>
2020-12-22 16:48:39 +0300

    MODORDSTOR-211 Cannot enable module for tenant if uuid-ossp extension installed (#227)
    * MODORDSTOR-211 Cannot enable module for tenant if uuid-ossp extension installed

--- src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
+++ src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
@@ -1,7 +1,7 @@
-CREATE EXTENSION "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 INSERT INTO ${myuniversity}_${mymodule}.titles
-SELECT uuid_generate_v4(), jsonb_strip_nulls(jsonb_build_object('title', po_line.jsonb -> 'title', 'poLineId', po_line.id, 'instanceId', po_line.jsonb-> 'instanceId',
+SELECT public.uuid_generate_v4(), jsonb_strip_nulls(jsonb_build_object('title', po_line.jsonb -> 'title', 'poLineId', po_line.id, 'instanceId', po_line.jsonb-> 'instanceId',
   'productIds', po_line.jsonb -> 'details' -> 'productIds', 'contributors', po_line.jsonb -> 'contributors', 'edition', po_line.jsonb -> 'edition',
   'publisher', po_line.jsonb -> 'publisher', 'publishedDate', po_line.jsonb -> 'publicationDate', 'subscriptionFrom', po_line.jsonb -> 'details' -> 'subscriptionFrom',
   'subscriptionTo', po_line.jsonb -> 'details' -> 'subscriptionTo', 'subscriptionInterval', po_line.jsonb -> 'details' -> 'subscriptionInterval'))
@@ -11,6 +11,3 @@ WHERE po_line.jsonb ? 'title';
 UPDATE ${myuniversity}_${mymodule}.po_line
 SET jsonb = jsonb - '{title}'::text[] || jsonb_build_object('titleOrPackage', jsonb->'title', 'isPackage', false)
 WHERE jsonb ? 'title';
-
-DROP EXTENSION "uuid-ossp";
-
