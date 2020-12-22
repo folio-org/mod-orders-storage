c2b843810039b0e5401175e864675a2afd5b8250
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-12-22 16:37:32 +0300

    MODORDSTOR-211 Cannot enable module for tenant if uuid-ossp extension installed

--- src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
+++ src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
@@ -1,4 +1,4 @@
-CREATE EXTENSION "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 INSERT INTO ${myuniversity}_${mymodule}.titles
 SELECT uuid_generate_v4(), jsonb_strip_nulls(jsonb_build_object('title', po_line.jsonb -> 'title', 'poLineId', po_line.id, 'instanceId', po_line.jsonb-> 'instanceId',
@@ -11,6 +11,3 @@ WHERE po_line.jsonb ? 'title';
 UPDATE ${myuniversity}_${mymodule}.po_line
 SET jsonb = jsonb - '{title}'::text[] || jsonb_build_object('titleOrPackage', jsonb->'title', 'isPackage', false)
 WHERE jsonb ? 'title';
-
-DROP EXTENSION "uuid-ossp";
-
