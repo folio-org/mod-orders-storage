2e109253e23f123386670300aad3f0c7c5522715
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-12-22 16:42:52 +0300

    MODORDSTOR-211 Cannot enable module for tenant if uuid-ossp extension installed

--- src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
+++ src/main/resources/templates/db_scripts/data-migration/10.0.0/po_line_table.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
 INSERT INTO ${myuniversity}_${mymodule}.titles
-SELECT uuid_generate_v4(), jsonb_strip_nulls(jsonb_build_object('title', po_line.jsonb -> 'title', 'poLineId', po_line.id, 'instanceId', po_line.jsonb-> 'instanceId',
+SELECT public.uuid_generate_v4(), jsonb_strip_nulls(jsonb_build_object('title', po_line.jsonb -> 'title', 'poLineId', po_line.id, 'instanceId', po_line.jsonb-> 'instanceId',
   'productIds', po_line.jsonb -> 'details' -> 'productIds', 'contributors', po_line.jsonb -> 'contributors', 'edition', po_line.jsonb -> 'edition',
   'publisher', po_line.jsonb -> 'publisher', 'publishedDate', po_line.jsonb -> 'publicationDate', 'subscriptionFrom', po_line.jsonb -> 'details' -> 'subscriptionFrom',
   'subscriptionTo', po_line.jsonb -> 'details' -> 'subscriptionTo', 'subscriptionInterval', po_line.jsonb -> 'details' -> 'subscriptionInterval'))
