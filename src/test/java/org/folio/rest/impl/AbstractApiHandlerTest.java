2d74f4ee516acca878ac1a4ebf1d51118e81da39
Andrei_Makaranka <Andrei_makaranka@epam.com>
2021-04-06 10:11:37 +0300

    [MODORDSTOR-141] - Add VertexContext to tests

--- src/test/java/org/folio/rest/impl/AbstractApiHandlerTest.java
+++ src/test/java/org/folio/rest/impl/AbstractApiHandlerTest.java
@@ -1,14 +1,11 @@
 package org.folio.rest.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.eq;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import org.folio.models.TableNames;
@@ -18,17 +15,20 @@ import org.folio.rest.persist.Tx;
 import org.folio.rest.persist.cql.CQLWrapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.handler.impl.HttpStatusException;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 import io.vertx.pgclient.PgConnection;
 import io.vertx.sqlclient.Query;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.Transaction;
 
-
+@ExtendWith(VertxExtension.class)
 public class AbstractApiHandlerTest {
 
   private PostgresClient postgresClient;
@@ -39,6 +39,7 @@ public class AbstractApiHandlerTest {
   private RowSet rowSet;
   private Tx<String> tx;
   private Query<RowSet<Row>> query;
+  private AbstractApiHandler abstractApiHandler;
 
   @BeforeEach
   void init() {
@@ -50,10 +51,16 @@ public class AbstractApiHandlerTest {
     rowSet = mock(RowSet.class);
     tx = mock(Tx.class);
     query = mock(Query.class);
+    abstractApiHandler = new AbstractApiHandler(postgresClient) {
+      @Override
+      String getEndpoint(Object entity) {
+        return "orders-storage/purchase-order";
+      }
+    };
   }
 
   @Test
-  void testShouldReturn400IfReturnZeroRecordAndSilentFalse() {
+  void testShouldReturn400IfReturnZeroRecordAndSilentFalse(VertxTestContext testContext) {
     when(pgConnection.query(any())).thenReturn(query);
     when(tx.getConnection()).thenReturn(asyncCon);
     when(asyncCon.result()).thenReturn(sqlConnection);
@@ -65,48 +72,42 @@ public class AbstractApiHandlerTest {
 
     doReturn(rowSet).when(asyncRowSet).result();
 
-    AbstractApiHandler abstractApiHandler = new AbstractApiHandler(postgresClient) {
-      @Override
-      String getEndpoint(Object entity) {
-        return "orders-storage/purchase-order";
-      }
-    };
     //Act
-    abstractApiHandler.deleteByQuery(tx, TableNames.PURCHASE_ORDER_TABLE, new CQLWrapper().setWhereClause("purchaseOrderId"), false)
-      .onComplete(asyncResult -> {
-            HttpStatusException exception = (HttpStatusException) asyncResult.cause();
-            assertEquals(404, exception.getStatusCode());
-    });
-  }
-
-  @Test
-  void testShouldReturnOriginTransactionIfReturnZeroRecordAndSilentTrue() {
-    when(pgConnection.query(any())).thenReturn(query);
-    when(tx.getConnection()).thenReturn(asyncCon);
-    when(asyncCon.result()).thenReturn(sqlConnection);
-    doAnswer(invocationOnMock -> {
-      Handler<AsyncResult<RowSet>> successHandler = invocationOnMock.getArgument(0);
-      successHandler.handle(asyncRowSet);
-      return null;
-    }).when(query).execute(any(Handler.class));
-
-    doReturn(rowSet).when(asyncRowSet).result();
-
-    AbstractApiHandler abstractApiHandler = new AbstractApiHandler(postgresClient) {
-      @Override
-      String getEndpoint(Object entity) {
-        return "orders-storage/purchase-order";
-      }
-    };
-    //Act
-    abstractApiHandler.deleteByQuery(tx, TableNames.PURCHASE_ORDER_TABLE, new CQLWrapper().setWhereClause("purchaseOrderId"), true)
-      .onComplete(asyncResult -> {
-          assertEquals(tx, asyncResult.result());
+    testContext.assertFailure(abstractApiHandler.deleteByQuery(tx, TableNames.PURCHASE_ORDER_TABLE, new CQLWrapper().setWhereClause("purchaseOrderId"), false))
+      .onComplete(event -> {
+        testContext.verify(() -> {
+          HttpStatusException exception = (HttpStatusException) event.cause();
+          assertEquals(404, exception.getStatusCode());
+        });
+        testContext.completeNow();
       });
   }
 
   @Test
-  void testShouldReturnOriginTransactionIfReturnNonZeroRecord() {
+  void testShouldReturnOriginTransactionIfReturnZeroRecordAndSilentTrue(VertxTestContext testContext) {
+    when(pgConnection.query(any())).thenReturn(query);
+    when(tx.getConnection()).thenReturn(asyncCon);
+    when(asyncCon.result()).thenReturn(sqlConnection);
+    doAnswer(invocationOnMock -> {
+      Handler<AsyncResult<RowSet>> successHandler = invocationOnMock.getArgument(0);
+      successHandler.handle(asyncRowSet);
+      return null;
+    }).when(query).execute(any(Handler.class));
+
+    doReturn(rowSet).when(asyncRowSet).result();
+
+    //Act
+    testContext.assertComplete(abstractApiHandler.deleteByQuery(tx, TableNames.PURCHASE_ORDER_TABLE, new CQLWrapper().setWhereClause("purchaseOrderId"), true))
+      .onComplete(event -> {
+        testContext.verify(() -> {
+          assertEquals(tx, event.result());
+        });
+        testContext.completeNow();
+      });
+  }
+
+  @Test
+  void testShouldReturnOriginTransactionIfReturnNonZeroRecord(VertxTestContext testContext) {
     when(pgConnection.query(any())).thenReturn(query);
     when(tx.getConnection()).thenReturn(asyncCon);
     when(asyncCon.result()).thenReturn(sqlConnection);
@@ -118,16 +119,14 @@ public class AbstractApiHandlerTest {
 
     doReturn(rowSet).when(asyncRowSet).result();
     doReturn(1).when(rowSet).rowCount();
-    AbstractApiHandler abstractApiHandler = new AbstractApiHandler(postgresClient) {
-      @Override
-      String getEndpoint(Object entity) {
-        return "orders-storage/purchase-order";
-      }
-    };
+
     //Act
-    abstractApiHandler.deleteByQuery(tx, TableNames.PURCHASE_ORDER_TABLE, new CQLWrapper().setWhereClause("purchaseOrderId"), true)
-      .onComplete(asyncResult -> {
-        assertEquals(tx, asyncResult.result());
+    testContext.assertComplete(abstractApiHandler.deleteByQuery(tx, TableNames.PURCHASE_ORDER_TABLE, new CQLWrapper().setWhereClause("purchaseOrderId"), true))
+      .onComplete(event -> {
+        testContext.verify(() -> {
+          assertEquals(tx, event.result());
+        });
+        testContext.completeNow();
       });
   }
 
