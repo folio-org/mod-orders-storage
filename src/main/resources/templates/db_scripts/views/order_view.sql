5a0ed6957ddf7253576260f0a4803a7c9ef752df
Siarhei Hrabko <grabsefx@gmail.com>
2021-05-24 02:20:51 +0300

    MODORDSTOR-229 Switch GET /orders-storage/orders search to cross-index approach

--- src/main/resources/templates/db_scripts/views/order_view.sql
+++
@@ -1,2 +0,0 @@
-CREATE OR REPLACE VIEW orders_view AS SELECT purchase_order.id AS id, purchase_order.jsonb AS jsonb, (COALESCE(po_line.jsonb, '{}'::jsonb) - 'tags') || purchase_order.jsonb AS metadata
-FROM purchase_order LEFT JOIN po_line ON (po_line.jsonb -> 'purchaseOrderId'::text) = (purchase_order.jsonb -> 'id'::text);
