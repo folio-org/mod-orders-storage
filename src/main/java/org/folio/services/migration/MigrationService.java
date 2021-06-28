bebb26990bda7adc774e1a86af9f2758cf2d1edd
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2021-06-28 11:43:10 +0300

    MODORDSTOR-233 fixed single quote processing upon migration (#254)
    * MODORDSTOR-233 fixed single quote processing upon migration
    
    (cherry picked from commit 8e693205919cbd05ddcface07d0651d595c91763)

--- src/main/java/org/folio/services/migration/MigrationService.java
+++ src/main/java/org/folio/services/migration/MigrationService.java
@@ -2,20 +2,22 @@ package org.folio.services.migration;
 
 import static org.folio.rest.persist.ResponseUtils.handleFailure;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import io.vertx.core.Context;
-import io.vertx.core.Future;
-import io.vertx.core.Promise;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.dbschema.ObjectMapperTool;
 import org.folio.rest.acq.model.finance.Fund;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.persist.DBClient;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.services.finance.FinanceService;
+
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
 
 public class MigrationService {
 
@@ -58,19 +60,32 @@ public class MigrationService {
     String schemaName = PostgresClient.convertToPsqlStandard(client.getTenantId());
     String sql = "DO\n" + "$$\n" + "begin\n" + " PERFORM %s.set_fund_code_into_pol('%s');\n" + "end;\n"
       + "$$ LANGUAGE plpgsql;";
-    try {
-      String json = ObjectMapperTool.getMapper().writeValueAsString(funds);
-      client.getPgClient()
-        .execute(String.format(sql, schemaName, json), event -> {
-          if (event.succeeded()) {
-            promise.complete();
-          } else {
-            handleFailure(promise, event);
-          }
-        });
-    } catch (JsonProcessingException e) {
-      promise.fail(e.getCause());
-    }
+
+    // get string representation of funds with conversion of single quotes in values into double single quotes
+    var jsonString = getFundsAsJsonString(funds);
+
+    client.getPgClient()
+      .execute(String.format(sql, schemaName, jsonString), event -> {
+        if (event.succeeded()) {
+          promise.complete();
+        } else {
+          handleFailure(promise, event);
+        }
+      });
     return promise.future();
+  }
+
+  private String getFundsAsJsonString(List<Fund> funds) {
+    var processedFunds = funds.stream()
+      .map(fund -> fund.withName(replaceSingleQuote(fund.getName()))
+        .withDescription(replaceSingleQuote(fund.getDescription()))
+        .withCode(replaceSingleQuote(fund.getCode())))
+      .collect(Collectors.toList());
+
+    return new JsonArray(processedFunds).encode();
+  }
+
+  public String replaceSingleQuote(String inputString) {
+    return inputString != null ? inputString.replace("'", "''") : null;
   }
 }
