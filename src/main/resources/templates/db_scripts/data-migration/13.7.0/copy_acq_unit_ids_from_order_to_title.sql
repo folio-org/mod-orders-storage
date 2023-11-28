0c676a4ccdadb2d6a5b5da4a86ceced3a6e5716e
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-11-28 12:38:55 +0400

    [MODORDSTOR-359] - Added explain analyze

--- src/main/resources/templates/db_scripts/data-migration/13.7.0/copy_acq_unit_ids_from_order_to_title.sql
+++ src/main/resources/templates/db_scripts/data-migration/13.7.0/copy_acq_unit_ids_from_order_to_title.sql
@@ -1,7 +1,7 @@
 UPDATE ${myuniversity}_${mymodule}.titles as titles
 SET jsonb = jsonb_set(titles.jsonb::jsonb, '{acqUnitIds}', purchase_order.jsonb->'acqUnitIds')
 FROM ${myuniversity}_${mymodule}.po_line as po_line, ${myuniversity}_${mymodule}.purchase_order as purchase_order
-WHERE titles.jsonb->>'poLineId' = po_line.jsonb->>'id'
-  AND po_line.jsonb->>'purchaseOrderId' = purchase_order.jsonb->>'id'
+WHERE lower(f_unaccent(titles.jsonb->>'poLineId')) = lower(f_unaccent(po_line.jsonb->>'id'))
+  AND lower(f_unaccent(po_line.jsonb->>'purchaseOrderId')) = lower(f_unaccent(purchase_order.jsonb->>'id'))
   AND purchase_order.jsonb->'acqUnitIds' != '[]'
   AND (titles.jsonb->'acqUnitIds' IS NULL OR titles.jsonb->'acqUnitIds' = '[]');
