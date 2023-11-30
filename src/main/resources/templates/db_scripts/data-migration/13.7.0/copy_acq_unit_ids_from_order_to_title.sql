42ede88e712904d2a6bb560a1645d8a5f47fb27c
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2023-11-30 14:30:19 +0400

    [MODORDSTOR-359] - Sql script updated

--- src/main/resources/templates/db_scripts/data-migration/13.7.0/copy_acq_unit_ids_from_order_to_title.sql
+++ src/main/resources/templates/db_scripts/data-migration/13.7.0/copy_acq_unit_ids_from_order_to_title.sql
@@ -1,7 +1,8 @@
-UPDATE ${myuniversity}_${mymodule}.titles as titles
-SET jsonb = jsonb_set(titles.jsonb::jsonb, '{acqUnitIds}', purchase_order.jsonb->'acqUnitIds')
+UPDATE ${myuniversity}_${mymodule}.titles as title
+SET jsonb = jsonb_set(title.jsonb::jsonb, '{acqUnitIds}', purchase_order.jsonb-> 'acqUnitIds')
 FROM ${myuniversity}_${mymodule}.po_line as po_line, ${myuniversity}_${mymodule}.purchase_order as purchase_order
-WHERE lower(f_unaccent(titles.jsonb->>'poLineId')) = lower(f_unaccent(po_line.jsonb->>'id'))
-  AND lower(f_unaccent(po_line.jsonb->>'purchaseOrderId')) = lower(f_unaccent(purchase_order.jsonb->>'id'))
-  AND purchase_order.jsonb->'acqUnitIds' != '[]'
-  AND (titles.jsonb->'acqUnitIds' IS NULL OR titles.jsonb->'acqUnitIds' = '[]');
+WHERE title.poLineId = po_line.id
+  AND po_line.purchaseOrderId = purchase_order.id
+  AND left(lower(f_unaccent(purchase_order.jsonb->>'acqUnitIds')), 600) NOT LIKE f_unaccent('[]')
+  AND (left(lower(f_unaccent(title.jsonb->>'acqUnitIds')), 600) IS NULL OR
+       left(lower(f_unaccent(title.jsonb->>'acqUnitIds')), 600) LIKE lower(f_unaccent('[]')));
