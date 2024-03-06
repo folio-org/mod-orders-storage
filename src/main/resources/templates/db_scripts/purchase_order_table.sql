0d0370a87c6410fcec6d01f4f3cf9dc5253bd916
alb3rtino <alb3rtino@users.noreply.github.com>
2024-03-06 23:44:23 +0100

    Add indexes for RecordService queries

--- src/main/resources/templates/db_scripts/purchase_order_table.sql
+++ src/main/resources/templates/db_scripts/purchase_order_table.sql
@@ -3,3 +3,6 @@ GRANT USAGE ON SEQUENCE ${myuniversity}_${mymodule}.po_number TO ${myuniversity}
 
 
 CREATE UNIQUE INDEX IF NOT EXISTS purchase_order_po_number_unique_idx ON ${myuniversity}_${mymodule}.purchase_order ((jsonb->>'poNumber'));
+CREATE INDEX IF NOT EXISTS purchase_order_customfields_recordservice_idx_gin
+ON ${myuniversity}_${mymodule}.purchase_order USING GIN ((jsonb->'customFields'));
+
