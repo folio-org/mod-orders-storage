3302e4aa6f2fabf12d815731abe1730a9edf7ed3
saba_zedginidze <saba_zedginidze@epam.com>
2024-03-23 02:43:11 +0400

    [MODORDSTOR-364] Create routing_list table and API for it

--- src/main/resources/templates/db_scripts/tables/create_routing_list_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE routing_list
-(
-    id uuid NOT NULL PRIMARY KEY,
-    name text NOT NULL UNIQUE,
-    notes text,
-    "userIds" uuid[] NOT NULL,
-    "poLineId" uuid NOT NULL,
-    FOREIGN KEY ("poLineId") REFERENCES po_line (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE
-);
