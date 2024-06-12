e0d04da9079c5d8d0cc0e2ef629fce5efa7e541e
alb3rtino <alb3rtino@users.noreply.github.com>
2024-06-12 16:34:41 +0200

    Add index management for custom fields

--- src/test/java/org/folio/StorageTestSuite.java
+++ src/test/java/org/folio/StorageTestSuite.java
@@ -30,6 +30,7 @@ import org.folio.rest.RestVerticle;
 import org.folio.rest.core.ResponseUtilTest;
 import org.folio.rest.impl.ClaimingAPITest;
 import org.folio.rest.impl.CustomFieldsAPITest;
+import org.folio.rest.impl.CustomFieldsIndexesTest;
 import org.folio.rest.impl.EntitiesCrudTest;
 import org.folio.rest.impl.EntititesCustomFieldsTest;
 import org.folio.rest.impl.HelperUtilsTest;
@@ -281,6 +282,8 @@ public class StorageTestSuite {
   class ClaimingAPITestNested extends ClaimingAPITest {}
   @Nested
   class CustomFieldsAPITestNested extends CustomFieldsAPITest {}
+  @Nested
+  class CustomFieldsIndexesTestNested extends CustomFieldsIndexesTest {}
   @Nested
   class EntititesCustomFieldsTestNested extends EntititesCustomFieldsTest {}
 
