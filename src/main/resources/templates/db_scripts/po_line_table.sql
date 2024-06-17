7194801f6e7bb4f00b4a9bda7af5017e65f519af
damien-git <damien-git@users.noreply.github.com>
2024-06-17 16:12:13 +0300

    [MODORDSTOR-406] Added new indices to speed up filtering from UI & Reference Data fixes

--- src/main/resources/templates/db_scripts/po_line_table.sql
+++ src/main/resources/templates/db_scripts/po_line_table.sql
@@ -1,2 +1,12 @@
-CREATE INDEX IF NOT EXISTS po_line_customfields_recordservice_idx_gin
-ON ${myuniversity}_${mymodule}.po_line USING GIN ((jsonb->'customFields'));
+CREATE INDEX IF NOT EXISTS po_line_customfields_recordservice_idx_gin ON ${myuniversity}_${mymodule}.po_line
+  USING GIN ((jsonb->'customFields'));
+
+CREATE INDEX IF NOT EXISTS po_line_updated_date_sort ON ${myuniversity}_${mymodule}.po_line
+  (left(lower(f_unaccent(jsonb->'metadata'->>'updatedDate')),600) DESC,
+    lower(f_unaccent(jsonb->'metadata'->>'updatedDate')) DESC);
+
+CREATE INDEX IF NOT EXISTS po_line_title_or_package_sort ON ${myuniversity}_${mymodule}.po_line
+  (left(lower(f_unaccent(jsonb->>'titleOrPackage')),600) DESC, lower(f_unaccent(po_line.jsonb->>'titleOrPackage')) DESC);
+
+CREATE INDEX IF NOT EXISTS po_line_po_line_number_sort ON ${myuniversity}_${mymodule}.po_line
+  (left(lower(f_unaccent(jsonb->>'poLineNumber')),600) DESC, lower(f_unaccent(po_line.jsonb->>'poLineNumber')) DESC);
