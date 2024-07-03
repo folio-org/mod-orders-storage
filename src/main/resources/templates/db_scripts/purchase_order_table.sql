7111fd95c086f498837e9c7cbcd129c3faeee490
Serhii_Nosko <Serhii_Nosko@epam.com>
2024-07-03 08:47:11 +0300

    MODORDSTOR-409. Add more indexes for sorting

--- src/main/resources/templates/db_scripts/purchase_order_table.sql
+++ src/main/resources/templates/db_scripts/purchase_order_table.sql
@@ -6,8 +6,3 @@ CREATE INDEX IF NOT EXISTS purchase_order_customfields_recordservice_idx_gin
 
 CREATE INDEX IF NOT EXISTS purchase_order_no_acq_unit ON ${myuniversity}_${mymodule}.purchase_order(jsonb)
   WHERE left(lower(f_unaccent(jsonb ->> 'acqUnitIds')), 600) NOT LIKE '[]';
-
-/* Use custom declaration of GIN index, because if to define it in schema.json RMB builds another SQL query
-   that executes longer for current case */
-CREATE INDEX IF NOT EXISTS purchase_order_workflowstatus_idx_gin ON diku_mod_orders_storage.purchase_order
-  USING gin (lower(diku_mod_orders_storage.f_unaccent((jsonb ->> 'workflowStatus'::text))) gin_trgm_ops);
--- src/main/resources/templates/db_scripts/title_table.sql
+++ src/main/resources/templates/db_scripts/title_table.sql
@@ -3,3 +3,9 @@ CREATE INDEX IF NOT EXISTS titles_title_sort ON ${myuniversity}_${mymodule}.titl
 
 CREATE INDEX IF NOT EXISTS titles_po_line_number_sort ON ${myuniversity}_${mymodule}.titles
   (left(lower(f_unaccent(titles.jsonb->>'poLineNumber')), 600), lower(f_unaccent(titles.jsonb->>'poLineNumber')));
+
+CREATE INDEX IF NOT EXISTS titles_receiving_note_sort ON ${myuniversity}_${mymodule}.titles
+  (left(lower(f_unaccent(titles.jsonb->'poLine'->>'receivingNote')), 600), lower(f_unaccent(titles.jsonb->'poLine'->>'receivingNote')));
+
+CREATE INDEX IF NOT EXISTS titles_package_sort ON ${myuniversity}_${mymodule}.titles
+  (left(lower(f_unaccent(titles.jsonb->'poLine'->>'titleOrPackage')), 600), lower(f_unaccent(titles.jsonb->'poLine'->>'titleOrPackage')));
