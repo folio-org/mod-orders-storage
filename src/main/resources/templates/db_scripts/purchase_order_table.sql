bbb0fb6a9964bdc38a1090e500e648fcf2cf135d
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-07-29 13:59:28 +0500

    [MODORDSTOR_IDX_FIX]. Remove faulty purchase_order_no_acq_unit index (#429)
    (cherry picked from commit a4410ac952b99710f956b46e9ce07e7ddeea8078)

--- src/main/resources/templates/db_scripts/purchase_order_table.sql
+++ src/main/resources/templates/db_scripts/purchase_order_table.sql
@@ -1,10 +1,8 @@
 CREATE SEQUENCE IF NOT EXISTS ${myuniversity}_${mymodule}.po_number MAXVALUE 9999999999999999 START WITH 10000 CACHE 1 NO CYCLE;
 GRANT USAGE ON SEQUENCE ${myuniversity}_${mymodule}.po_number TO ${myuniversity}_${mymodule};
 
+CREATE UNIQUE INDEX IF NOT EXISTS purchase_order_po_number_unique_idx ON ${myuniversity}_${mymodule}.purchase_order
+  ((jsonb->>'poNumber'));
 
-CREATE UNIQUE INDEX IF NOT EXISTS purchase_order_po_number_unique_idx ON ${myuniversity}_${mymodule}.purchase_order ((jsonb->>'poNumber'));
 CREATE INDEX IF NOT EXISTS purchase_order_customfields_recordservice_idx_gin
-ON ${myuniversity}_${mymodule}.purchase_order USING GIN ((jsonb->'customFields'));
-
-CREATE INDEX IF NOT EXISTS purchase_order_no_acq_unit ON ${myuniversity}_${mymodule}.purchase_order(jsonb)
-  WHERE left(lower(f_unaccent(jsonb ->> 'acqUnitIds')), 600) NOT LIKE '[]';
+  ON ${myuniversity}_${mymodule}.purchase_order USING GIN ((jsonb->'customFields'));
