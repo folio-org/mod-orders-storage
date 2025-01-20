3fbbce91a0d180e63131ad4cf420cfa3fdbcc906
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-01-20 18:06:53 +0500

    [MODORDERS-1237]. Create a new API and wrapper model for Claiming with an Organization Id

---
+++ src/main/resources/templates/db_scripts/views/wrapper_pieces_view.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.wrapper_pieces_view AS
+select t.id,
+			 jsonb_build_object(
+         'vendorId', (d.jsonb->>'vendor'),
+       	 'piece', t.jsonb) jsonb,
+			 t.creation_date,
+			 t.created_by,
+			 t.polineid,
+			 t.titleid
+  from ${myuniversity}_${mymodule}.pieces t
+    join ${myuniversity}_${mymodule}.po_line c on c.id = t.polineid
+    join ${myuniversity}_${mymodule}.purchase_order d on d.id = c.purchaseorderid
+;
