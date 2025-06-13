768c200b638d0a849aef7b8f2c2b92334c64b9a8
saba_zedginidze <saba_zedginidze@epam.com>
2025-06-13 15:45:08 +0400

    [MODORDSTOR-454] Delete unnecessary service

--- src/main/java/org/folio/util/ResourcePath.java
+++ src/main/java/org/folio/util/ResourcePath.java
@@ -11,7 +11,8 @@ public enum ResourcePath {
   USER_TENANTS_ENDPOINT("/user-tenants"),
   STORAGE_HOLDING_URL("/holdings-storage/holdings"),
   STORAGE_BATCH_HOLDING_URL("/holdings-storage/batch/synchronous"),
-  STORAGE_INSTANCE_URL("/instance-storage/instances/%s");
+  STORAGE_INSTANCE_URL("/instance-storage/instances/%s"),
+  SETTINGS_URL("/settings/entries"),;
 
   private final String path;
 }
