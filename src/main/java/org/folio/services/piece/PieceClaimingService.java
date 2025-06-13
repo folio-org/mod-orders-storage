51f34043ed0d14e6cb9ef82e7c30eeb77812c2f0
saba_zedginidze <saba_zedginidze@epam.com>
2025-06-13 15:44:56 +0400

    [MODORDSTOR-454] Replace tenant locale settings service

--- src/main/java/org/folio/services/piece/PieceClaimingService.java
+++ src/main/java/org/folio/services/piece/PieceClaimingService.java
@@ -2,39 +2,38 @@ package org.folio.services.piece;
 
 import io.vertx.core.Context;
 import io.vertx.core.Future;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.folio.dao.ExtensionRepository;
 import org.folio.dao.PieceClaimingRepository;
 import org.folio.dao.PostgresClientFactory;
+import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.tools.utils.TenantTool;
-import org.folio.services.configuration.TenantLocaleSettingsService;
+import org.folio.services.setting.CommonSettingsService;
 
 import java.util.Map;
 
+@Log4j2
 public class PieceClaimingService {
-  private static final Logger log = LogManager.getLogger();
-
 
   private final PostgresClientFactory pgClientFactory;
   private final ExtensionRepository extensionRepository;
   private final PieceClaimingRepository pieceClaimingRepository;
-  private final TenantLocaleSettingsService tenantLocaleSettingsService;
+  private final CommonSettingsService commonSettingsService;
 
   public PieceClaimingService(PostgresClientFactory pgClientFactory, ExtensionRepository extensionRepository,
-                              PieceClaimingRepository pieceClaimingRepository, TenantLocaleSettingsService tenantLocaleSettingsService) {
+                              PieceClaimingRepository pieceClaimingRepository, CommonSettingsService commonSettingsService) {
     this.pgClientFactory = pgClientFactory;
     this.extensionRepository = extensionRepository;
     this.pieceClaimingRepository = pieceClaimingRepository;
-    this.tenantLocaleSettingsService = tenantLocaleSettingsService;
+    this.commonSettingsService = commonSettingsService;
   }
 
   public Future<Integer> processClaimedPieces(Map<String, String> okapiHeaders, Context vertxContext) {
     String tenantId = TenantTool.tenantId(okapiHeaders);
     log.info("processClaimedPieces, tenantId={}", tenantId);
     PostgresClient pgClient = pgClientFactory.createInstance(tenantId);
-    return tenantLocaleSettingsService.getTenantTimeZone(okapiHeaders, vertxContext)
+    return commonSettingsService.getTenantTimeZone(new RequestContext(vertxContext, okapiHeaders))
         .compose(tenantTimeZone -> pgClient.withConn(conn -> extensionRepository.createUUIDExtension(conn)
           .compose(aVoid -> pieceClaimingRepository.updatePieceStatusBasedOnIntervals(conn, tenantId, tenantTimeZone))));
   }
