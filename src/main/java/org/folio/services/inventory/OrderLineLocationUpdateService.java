40d2fe7bce928514dc5902ec5f6caf08ab1084e9
saba_zedginidze <saba_zedginidze@epam.com>
2025-07-25 16:14:46 +0400

    Revert "[MODORDERS-1299] Handle missing POLs update"
    This reverts commit f94fc2701b1784af0df7cbaec0745fa1b06b55eb.

--- src/main/java/org/folio/services/inventory/OrderLineLocationUpdateService.java
+++ src/main/java/org/folio/services/inventory/OrderLineLocationUpdateService.java
@@ -75,9 +75,8 @@ public class OrderLineLocationUpdateService {
     log.info("repairPoLineLocationData:: Fetching '{}' POL(s) to repair location data", poLineIds.size());
     return poLinesService.getPoLinesByIdsForUpdate(poLineIds, tenantId, conn)
       .map(poLines -> repairPoLineLocations(poLines, holdingIdPair))
-      .compose(poLinesToUpdate -> CollectionUtils.isEmpty(poLinesToUpdate)
-        ? Future.succeededFuture(List.of())
-        : poLinesService.updatePoLines(poLinesToUpdate, conn, tenantId, headers).map(v -> poLinesToUpdate));
+      .compose(poLinesToUpdate -> poLinesService.updatePoLines(poLinesToUpdate, conn, tenantId, headers)
+        .map(v -> poLinesToUpdate));
   }
 
   private Future<List<PoLine>> updatePoLines(List<Pair<PoLine, List<Piece>>> poLinePiecePairs, JsonObject item,
@@ -155,10 +154,9 @@ public class OrderLineLocationUpdateService {
         }
       });
       if (locations.equals(poLine.getLocations())) {
+        log.info("repairPoLineLocations:: No changes were made to POL: '{}'", poLine.getId());
         return false;
       }
-      log.info("repairPoLineLocations:: Replacing locations of POL: '{}' having old value: '{}' with new value: '{}'",
-        poLine.getId(), JsonArray.of(poLine.getLocations()).encode(), JsonArray.of(locations).encode());
       poLine.setLocations(locations);
       return true;
     }).toList();
