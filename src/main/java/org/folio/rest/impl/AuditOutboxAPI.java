728af4f677d956d204ca6407ca43d5e23023fe12
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-01-29 18:19:25 +0500

    [MODORDERS-1372]. Fix sonar issues

--- src/main/java/org/folio/rest/impl/AuditOutboxAPI.java
+++ src/main/java/org/folio/rest/impl/AuditOutboxAPI.java
@@ -5,8 +5,6 @@ import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.folio.event.service.AuditOutboxService;
 import org.folio.rest.jaxrs.resource.OrdersStorageAuditOutbox;
 import org.folio.spring.SpringContextUtil;
@@ -16,8 +14,6 @@ import javax.ws.rs.core.Response;
 import java.util.Map;
 
 public class AuditOutboxAPI implements OrdersStorageAuditOutbox {
-
-  private static final Logger log = LogManager.getLogger();
 
   @Autowired
   private AuditOutboxService auditOutboxService;
